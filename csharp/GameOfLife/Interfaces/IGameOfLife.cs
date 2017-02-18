e85c549a2ffe1ccf60f30e205a9687da82f6623c
Taylor Harry <taylor.andrew.harry@gmail.com>
2017-02-18 09:38:15 -0800

    added initial test classes and interface

---
+++ csharp/GameOfLife/Interfaces/IGameOfLife.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Interfaces
+{
+    interface IGameOfLife
+    {
+        void Reproduction(object input);
+        void UnderPopulation(object input);
+        void OverPopulation(object input);
+        void LiveOn(object input);
+    }
+}
---
+++ csharp/GameOfLifeTests/GameOfLifeTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class GameOfLifeTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateTheWorld()
+        {
+        }
+
+        [TestMethod]
+        public void EnsureReproductionWithStillLife()
+        {
+
+        }
+
+        [TestMethod]
+        public void EnsureOverPopulationWithStillLIfe()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void EnsureUnderPopulationWithStillLIfe()
+        {
+
+        }
+
+        [TestMethod]
+        public void EnsureLiveOnWithStillLife()
+        {
+
+        }
+
+        [TestMethod]
+        public void EnsureReproductionWithOscillator()
+        {
+
+        }
+
+        [TestMethod]
+        public void EnsureOverPopulationWithOscillator()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void EnsureUnderPopulationWithOscillator()
+        {
+
+        }
+    }
+}
